948d6d53703b26195656096c89ddd43bfc054e58
ambuj vashistha <ambujva123@gmail.com>
2026-03-19 20:54:48 +0530

    let's go

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -1,35 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import InfiniteMenu from './components/infiniteMenu'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
+  const items = [
+    {
+      image: "https://picsum.photos/300/300?grayscale",
+      link: "https://google.com/",
+      title: "Item 1",
+      description: "This is pretty cool, right?",
+    },
+    {
+      image: "https://picsum.photos/400/400?grayscale",
+      link: "https://google.com/",
+      title: "Item 2",
+      description: "This is pretty cool, right?",
+    },
+    {
+      image: "https://picsum.photos/500/500?grayscale",
+      link: "https://google.com/",
+      title: "Item 3",
+      description: "This is pretty cool, right?",
+    },
+    {
+      image: "https://picsum.photos/600/600?grayscale",
+      link: "https://google.com/",
+      title: "Item 4",
+      description: "This is pretty cool, right?",
+    },
+  ];
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <InfiniteMenu items={items} />
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
